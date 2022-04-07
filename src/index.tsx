54052b4a5806da059befea51b54a6deabf196d91
fukmats <fukmats@gmail.com>
2022-04-07 09:51:42 +0900

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from "react-router-dom"
 import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import reportWebVitals from './reportWebVitals';
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+       <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const CONTENTS = gql`
+    query getContents {
+        contentForHome {
+            id
+            title
+            thumbnail
+        }
+    }
+`;
+
+
+/* 
+* Home page 
+* We display contents 
+*/
+const Homepage = ()  => {
+    const { loading, error, data } = useQuery(CONTENTS);
+    console.log();
+    if (loading) return <p>"Loading..."</p>;
+
+    if (error) return <p>`Error! ${error.message}`</p>;
+
+    return (
+        <p>{ JSON.stringify(data) }</p>
+    );
+};
+
+export default Homepage;
