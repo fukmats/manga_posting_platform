61f5b95e75dfb8ec589d288056657115038a00f6
fukmats <fukmats@gmail.com>
2022-04-09 12:29:57 +0900

    add server:

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { useRoutes } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-
-function App() {
-  let routes = useRoutes([{ path: "/", element: <Homepage /> }]);
-  return routes;
-}
-
-export default App;
