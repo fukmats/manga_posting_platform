23a629b0c7cf87a6c14ca138c1bed417dad6db42
fukmats <fukmats@gmail.com>
2022-04-09 16:03:32 +0900

    add graphql schema

---
+++ server/src/schema.js
@@ -0,0 +1,41 @@
+const { gql } = require('apollo-server');
+const typeDefs = gql`
+    type Category {
+        categoryId: ID!
+        categoryName: String!
+        contents: [Content!]!
+    }
+    "Information to be displayed on the homepage"
+    type Content {
+        contentId: ID!
+        language: String!
+        "Thumbnail image must include the title logo"
+        thumbnail: String!
+        "Text information is not displayed on the homepage"
+        contentInfo: [ContentInfo!]!
+    }
+    "Character information"
+    type ContentInfo {
+        ContentId: ID!
+        title: String!
+        year: Int!
+        numberOfVolumes: Int
+        creator: String!
+        categoryID:ID!
+        tags: [String]
+        volumes: [Volume]
+    }
+    type Volume {
+        volumeId: ID!
+        thumbnail:String!
+        duration: Float
+        plot: String
+        numberOfVolume: Int
+        images: [Image!]!
+    }
+    type Image {
+        imageId: ID!
+        image: String!
+    }
+`;
+module.exports = typeDefs;
