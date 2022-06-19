9c0c0132a98cd5586fc8a9fdcd22851dfc5481d2
fukmats <fukmats@gmail.com>
2022-06-19 13:50:39 +0900

    delete amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "niji": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
